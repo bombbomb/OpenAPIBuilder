57bcedd62f296d5575fcacf59935894a462e16f5
Patrick McDavid <fusionjunky@gmail.com>
2016-08-24 23:44:18 -0600

    Minor update

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+    grunt.initConfig({
+        shell: {
+            multiple: {
+                command: [
+                    'java -jar build/swagger-codegen-cli.jar generate  -i https://dev.api.bombbomb.com/v2/spec  -l javascript  -o build/nodejs -c config/nodejs.json',
+                    'sh build/nodejs/git_push.sh'
+                ].join('&&')
+            }
+        }
+    });
+
+
+    grunt.registerTask('default', ['shell']);
+};
