63a6349ee54905d0981022b25825c8c3024661bb
Patrick McDavid <fusionjunky@gmail.com>
2016-08-26 23:42:48 -0600

    bs

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ git remote add origin https://bombbomb:${GIT_TOKEN}@github.com/bombbomb/bombbomb
 git pull origin master
 find -delete
 
-java -jar swagger-codegen-cli.jar generate -i https://dev.api.bombbomb.com/v2/spec -l javascript -o ../javascript -c config/javascript.json
-cd ../javascript
+java -jar ../OpenAPIBuilder/swagger-codegen-cli.jar generate -i https://dev.api.bombbomb.com/v2/spec -l javascript -c ../OpenAPIBuilder/config/javascript.json
+
 chmod u+x git_push.sh
 sh git_push.sh bombbomb bombbomb-nodejs-openapi "ride the snake!"
+
