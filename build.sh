fc85bdca5d89f7b2569cef0295a152214c0239fb
Patrick McDavid <fusionjunky@gmail.com>
2016-08-26 23:38:39 -0600

    bs

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+echo "Ride the Snake!"
 pwd
 
 git config --global user.name "Patrick McDavid"
 git config --global user.email "patrick@bombbomb.com"
 
 # javascript
+mkdir ../javascript
+cd ../javascript
+git init
+git remote add origin https://bombbomb:${GIT_TOKEN}@github.com/bombbomb/bombbomb-nodejs-openapi.git
+git pull origin master
+find -delete
+
 java -jar swagger-codegen-cli.jar generate -i https://dev.api.bombbomb.com/v2/spec -l javascript -o ../javascript -c config/javascript.json
 cd ../javascript
 chmod u+x git_push.sh
