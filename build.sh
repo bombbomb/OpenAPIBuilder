e0d1272b48eed67759943657c11ceca647d4b127
Patrick McDavid <fusionjunky@gmail.com>
2016-08-26 23:23:23 -0600

    hax

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ git config --global user.email "patrick@bombbomb.com"
 java -jar swagger-codegen-cli.jar generate -i https://dev.api.bombbomb.com/v2/spec -l javascript -o ../javascript -c config/javascript.json
 cd ../javascript
 chmod u+x git_push.sh
-sh git_push.sh ehippy testspam "ride the snake!"
+sh git_push.sh bombbomb bombbomb-nodejs-openapi "ride the snake!"
