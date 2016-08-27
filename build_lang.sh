3f87e722beaa0e2e77443de209e04426031cf596
Patrick McDavid <fusionjunky@gmail.com>
2016-08-27 00:31:44 -0600

    maybe?

--- build_lang.sh
+++ build_lang.sh
@@ -23,7 +23,7 @@ ls
 echo "Pushing $language repo back to GitHub..."
 git add -A
 git status
-git push origin master -f
+git push origin master --force --verbose
 
 echo "Done generating $language client... resetting to start dir"
 cd ../OpenAPIBuilder
