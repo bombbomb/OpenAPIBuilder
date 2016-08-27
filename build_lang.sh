f602176117a08ca2e676ffd8629e6ae42169c8c4
Patrick McDavid <fusionjunky@gmail.com>
2016-08-27 00:23:29 -0600

    maybe?

--- build_lang.sh
+++ build_lang.sh
@@ -14,16 +14,16 @@ git init
 git remote add origin https://bombbomb:${GIT_TOKEN}@github.com/bombbomb/bombbomb-${language}-openapi.git
 git pull origin master
 git rm *
-
+git status
 
 echo "Generating $language OpenAPI client..."
 java -jar ../OpenAPIBuilder/swagger-codegen-cli.jar generate -i https://dev.api.bombbomb.com/v2/spec -l ${language} -c ../OpenAPIBuilder/config/${language}.json
 ls
 
 echo "Pushing $language repo back to GitHub..."
-git add .
+git add -A
+git status
 git push origin master -f
-
 
 echo "Done generating $language client... resetting to start dir"
 cd ../OpenAPIBuilder
