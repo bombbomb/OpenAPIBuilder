7d4a7d8bdc043077cf53bdb889ccde779c3a2075
thomas.pendergrass@bombbomb.com <thomas.pendergrass@bombbomb.com>
2018-07-30 15:08:52 -0600

    Parse version and add offset. SVN numbers were in the 20,000's while github numbers were in the 800's. ensure newer versions appear above older version in package managers.

--- getVersion.rb
+++ getVersion.rb
@@ -1,6 +1,17 @@
 require 'open-uri'
 require 'json'
 
+def addVersionOffset(version)
+	versionComponents = version.split('.')
+	svn_to_github_offset = 24967
+	patchNumber = versionComponents[2].to_i
+	
+	versionComponents[2] = (patchNumber + svn_to_github_offset).to_s
+	return versionComponents.join('.')
+end
+
 json_object = JSON.load(open("https://api.bombbomb.com/v2/spec").read)
 
-puts json_object['info']['version']
+version = addVersionOffset(json_object['info']['version'])
+
+puts version
